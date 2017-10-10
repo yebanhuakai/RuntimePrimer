52716f5fe7955d177e7d9902eb154e7f30eae54e
meng.yue <yebanhuakai@qq.com>
2017-10-10 10:53:12 +0800

    Initial Commit

---
+++ RuntimePrimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimePrimer
+//
+//  Created by Shenry on 2017/10/10.
+//  Copyright © 2017年 Shenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RuntimePrimer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuntimePrimer
+//
+//  Created by Shenry on 2017/10/10.
+//  Copyright © 2017年 Shenry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
